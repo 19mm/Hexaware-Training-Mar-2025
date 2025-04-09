fb28fe43c7126e4cb2ef640af2774fba0d6a21d6
Mayuresh Firodiya <92072778+19mm@users.noreply.github.com>
2025-04-09 16:09:30 +0530

    Update DBConnUtil.java

--- SIS_Project/src/com/hexaware/sis/main/util/DBConnUtil.java
+++ SIS_Project/src/com/hexaware/sis/main/util/DBConnUtil.java
@@ -7,11 +7,12 @@ import java.sql.SQLException;
 public class DBConnUtil {
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // Ensure the driver is loaded
+            //load the DB Driver
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
         } catch (ClassNotFoundException e) {
             throw new SQLException("JDBC Driver not found: " + e.getMessage(), e);
         }
-        // Attempt to get a connection using properties from DBPropertyUtil.
+        // Get connection using properties from DBPropertyUtil.
         Connection connection = DriverManager.getConnection(
             DBPropertyUtil.getConnectionString(),
             DBPropertyUtil.getUsername(),
