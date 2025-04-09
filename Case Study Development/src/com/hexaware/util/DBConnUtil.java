c269e69c088abb9aa87259daf8e7c30168566bde
Mayuresh Firodiya <92072778+19mm@users.noreply.github.com>
2025-04-09 14:28:09 +0530

    Update DBConnUtil.java

--- Case Study Development/src/com/hexaware/util/DBConnUtil.java
+++ Case Study Development/src/com/hexaware/util/DBConnUtil.java
@@ -4,12 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// DB Connection Establishment based on db.properties file
+
 public class DBConnUtil {
     private static Connection connection;
-
-    /**
-     * Returns a singleton Connection object.
-     */
+    
     public static Connection getConnection(String connectionString) {
         if (connection == null) {
             try {
