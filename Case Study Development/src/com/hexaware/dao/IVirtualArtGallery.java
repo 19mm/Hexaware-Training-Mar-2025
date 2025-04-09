9b849defa709f0a2ea7f7f7a54c87e066ed35cb3
Mayuresh Firodiya <92072778+19mm@users.noreply.github.com>
2025-04-09 14:18:07 +0530

    Update IVirtualArtGallery.java

--- Case Study Development/src/com/hexaware/dao/IVirtualArtGallery.java
+++ Case Study Development/src/com/hexaware/dao/IVirtualArtGallery.java
@@ -5,14 +5,12 @@ import java.util.List;
 import com.hexaware.exception.*;
 
 public interface IVirtualArtGallery {
-    // Artwork Management
+    //Declaration of Methods
     boolean addArtwork(Artwork artwork);
     boolean updateArtwork(Artwork artwork);
     boolean removeArtwork(int artworkId);
     Artwork getArtworkById(int artworkId) throws ArtWorkNotFoundException;
     List<Artwork> searchArtworks(String keyword);
-
-    // User Favorites
     boolean addArtworkToFavorite(int userId, int artworkId);
     boolean removeArtworkFromFavorite(int userId, int artworkId);
     List<Artwork> getUserFavoriteArtworks(int userId);
